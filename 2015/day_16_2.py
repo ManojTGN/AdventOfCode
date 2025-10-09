5716c50446f55981be6196d60d44d570b2fd4b2a
Manoj A <42494649+ManojTGN@users.noreply.github.com>
2025-10-09 15:34:31 +0530

    complete day 16 2015

---
+++ 2015/day_16_2.py
@@ -0,0 +1,92 @@
+'''
+--- Day 16: Aunt Sue ---
+Your Aunt Sue has given you a wonderful gift, and you'd like to send her a thank you card. However, there's a small problem: she signed it "From, Aunt Sue".
+
+You have 500 Aunts named "Sue".
+
+So, to avoid sending the card to the wrong person, you need to figure out which Aunt Sue (which you conveniently number 1 to 500, for sanity) gave you the gift. You open the present and, as luck would have it, good ol' Aunt Sue got you a My First Crime Scene Analysis Machine! Just what you wanted. Or needed, as the case may be.
+
+The My First Crime Scene Analysis Machine (MFCSAM for short) can detect a few specific compounds in a given sample, as well as how many distinct kinds of those compounds there are. According to the instructions, these are what the MFCSAM can detect:
+
+children, by human DNA age analysis.
+cats. It doesn't differentiate individual breeds.
+Several seemingly random breeds of dog: samoyeds, pomeranians, akitas, and vizslas.
+goldfish. No other kinds of fish.
+trees, all in one group.
+cars, presumably by exhaust or gasoline or something.
+perfumes, which is handy, since many of your Aunts Sue wear a few kinds.
+In fact, many of your Aunts Sue have many of these. You put the wrapping from the gift into the MFCSAM. It beeps inquisitively at you a few times and then prints out a message on ticker tape:
+
+children: 3
+cats: 7
+samoyeds: 2
+pomeranians: 3
+akitas: 0
+vizslas: 0
+goldfish: 5
+trees: 3
+cars: 2
+perfumes: 1
+You make a list of the things you can remember about each Aunt Sue. Things missing from your list aren't zero - you simply don't remember the value.
+
+What is the number of the Sue that got you the gift?
+--- Part Two ---
+As you're about to send the thank you note, something in the MFCSAM's instructions catches your eye. Apparently, it has an outdated retroencabulator, and so the output from the machine isn't exact values - some of them indicate ranges.
+
+In particular, the cats and trees readings indicates that there are greater than that many (due to the unpredictable nuclear decay of cat dander and tree pollen), while the pomeranians and goldfish readings indicate that there are fewer than that many (due to the modial interaction of magnetoreluctance).
+
+What is the number of the real Aunt Sue?
+'''
+
+import req
+import utils
+
+MAX_MATCH = 0
+GLOBAL_DATA = {}
+
+def main():
+    global MAX_MATCH
+    input_data = utils.getInputLines(2015,16)
+    ticker_tape = {
+        "children": 3,
+        "cats": 7,
+        "samoyeds": 2,
+        "pomeranians": 3,
+        "akitas": 0,
+        "vizslas": 0,
+        "goldfish": 5,
+        "trees": 3,
+        "cars": 2,
+        "perfumes": 1
+    }
+    
+    for data in input_data:
+        sdata = data.split(' ',2)
+        name = ' '.join(sdata[:2])[:-1]
+        GLOBAL_DATA[name] = {'data':{}}
+        rdata = sdata[-1].split(', ')
+        for d in rdata:
+            spd = d.split(': ')
+            GLOBAL_DATA[name]['data'][spd[0]] = int(spd[-1])
+    
+    NAME = ''
+    for name,data in GLOBAL_DATA.items():
+        match = 0
+        for item_name,item_data in data['data'].items():
+            if(item_name == 'cats' or item_name == 'trees'):
+                if item_name in ticker_tape and item_data > ticker_tape[item_name]:
+                    match += 1
+            elif(item_name == 'goldfish' or item_name == 'pomeranians'):
+                if item_name in ticker_tape and item_data < ticker_tape[item_name]:
+                    match += 1
+            elif(item_name in ticker_tape and item_data == ticker_tape[item_name]):
+                match+=1
+        if(match > MAX_MATCH):
+            MAX_MATCH = match
+            NAME = name
+    
+    print(NAME.split(' ')[-1])
+
+
+if __name__ == '__main__':
+    main()
